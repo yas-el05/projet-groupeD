65c1b1002b501fb4f96832ea634598a3cb8ae3eb
EL MRAH Yasmine <elmrahyasmine@MacBook-de-Yasmine.local>
2026-06-03 16:00:44 +0200

    ajout de l'interface pathFinder

--- src/evacuation/routing/PathFinder.java
+++ src/evacuation/routing/PathFinder.java
@@ -1,1 +1,20 @@
 package evacuation.routing;
+
+import evacuation.graph.Node;
+import java.util.List;
+
+public interface PathFinder {
+
+    /**
+     * Calcule le chemin le plus court entre src et dest
+     * @return liste ordonnée de nœuds (src inclus, dest inclus),
+     * ou liste vide si aucun chemin n'existe
+     */
+    List<Node> calculerChemin(Node src, Node dest); 
+
+    // Recalcule les distances internes (appeler après modification du graphe)
+    void recalculer();
+
+    // Retourne le dernier chemin calculé
+    List<Node> getPlusCourt();
+}
